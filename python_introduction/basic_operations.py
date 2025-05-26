33d41df061ccc55ae72274ec4f4535180b7bf1c7
Aguigbo emmanuel <aguigbochidera@gmail.com>
2025-05-26 01:14:33 +0100

    finally

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,12 @@
+number1 = 5
+number2 = 10
+# addiction
+sum = number1 + number2
+# subtraction
+difference = number2 - number1
+# multiplication
+product = number1 * number2
+# print results
+print("Addition of", number2, "and", number1, "is", sum )
+print("subtraction of", number2, "and", number1, "is",  difference)
+print("multiplication of", number2, "and", number1, "is", product)
