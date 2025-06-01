790ac103e399fa13974162669fffc6d798e21513
Aguigbo emmanuel <aguigbochidera@gmail.com>
2025-06-01 23:47:22 +0100

    code fixed

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,12 +1,25 @@
-number1 = 5
-number2 = 10
-# addiction
-sum = number1 + number2
+# basic_operations.py
+
+number1 = 10
+number2 = 5
+
+# addition
+addition = number1 + number2
+
 # subtraction
-difference = number2 - number1
+subtraction = number1 - number2
+
 # multiplication
-product = number1 * number2
+multiplication = number1 * number2
+
+# division
+division = number1 / number2
+
 # print results
-print("Addition of", number2, "and", number1, "is", sum )
+print("Addition of", number1, "and", number2, "is", addition)
+print("Subtraction of", number1, "and", number2, "is", subtraction)
+print("Multiplication of", number1, "and", number2, "is", multiplication)
+print("Division of", number1, "by", number2, "is", division)
+
 print("subtraction of", number2, "and", number1, "is",  difference)
 print("multiplication of", number2, "and", number1, "is", product)
