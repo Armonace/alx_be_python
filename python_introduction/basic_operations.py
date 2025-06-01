11561ad44e6d7219191ab6a87b67b98089916096
Aguigbo emmanuel <aguigbochidera@gmail.com>
2025-06-01 23:50:56 +0100

    bugs fixed

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -4,22 +4,19 @@ number1 = 10
 number2 = 5
 
 # addition
-addition = number1 + number2
+sum = number1 + number2
 
 # subtraction
-subtraction = number1 - number2
+difference = number1 - number2
 
 # multiplication
-multiplication = number1 * number2
+product = number1 * number2
 
 # division
-division = number1 / number2
+quotient = number1 / number2
 
 # print results
-print("Addition of", number1, "and", number2, "is", addition)
-print("Subtraction of", number1, "and", number2, "is", subtraction)
-print("Multiplication of", number1, "and", number2, "is", multiplication)
-print("Division of", number1, "by", number2, "is", division)
-
-print("subtraction of", number2, "and", number1, "is",  difference)
-print("multiplication of", number2, "and", number1, "is", product)
+print("Addition of", number1, "and", number2, "is", sum)
+print("Subtraction of", number1, "and", number2, "is", difference)
+print("Multiplication of", number1, "and", number2, "is", product)
+print("Division of", number1, "by", number2, "is", quotient)
