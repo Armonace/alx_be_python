f75e43334f6f78e00859b2eb945f0920c9a755a2
Aguigbo emmanuel <aguigbochidera@gmail.com>
2025-06-15 22:42:15 +0100

    error solved

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,18 +5,15 @@ class BankAccount:
     def deposit(self, amount):
         if amount > 0:
             self.__account_balance += amount
-            print(f"Deposited: ${amount:.1f}")
-        else:
-            print("Deposit amount must be positive.")
+            return f"Deposited: ${amount:.1f}"
+        return "Deposit amount must be positive."
 
     def withdraw(self, amount):
         if amount <= self.__account_balance:
             self.__account_balance -= amount
-            print(f"Withdrew: ${amount:.1f}")
-            return True
-        else:
-            print("Insufficient funds.")
-            return False
+            return f"Withdrew: ${amount:.1f}"
+        return "Insufficient funds."
+
 
     def display_balance(self):
         print(f"Current Balance: ${self.__account_balance:.2f}")
