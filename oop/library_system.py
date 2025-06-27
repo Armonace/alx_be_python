909b7236a810f3a8e7c713c267e2bac6f28dfe44
Aguigbo emmanuel <aguigbochidera@gmail.com>
2025-06-27 11:02:37 +0100

    auto checker error corrected

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,16 +5,16 @@ class Book:
         self.title = title
         self.author = author
 
-    def get_details(self):
+    def __str__(self):
         return f"{self.title} by {self.author}"
 
 
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
-        self.file_size = file_size  # in MB
+        self.file_size = file_size
 
-    def get_details(self):
+    def __str__(self):
         return f"{self.title} by {self.author} [E-Book, {self.file_size}MB]"
 
 
@@ -23,13 +23,13 @@ class PrintBook(Book):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
+    def __str__(self):
         return f"{self.title} by {self.author} [Print Book, {self.page_count} pages]"
 
 
 class Library:
     def __init__(self):
-        self.books = []  # composition: contains Book, EBook, PrintBook instances
+        self.books = []
 
     def add_book(self, book):
         self.books.append(book)
@@ -38,4 +38,5 @@ class Library:
         if not self.books:
             print("The library has no books.")
         for book in self.books:
-            print(book.get_details())
+            print(book)  # __str__ is automatically called
+
